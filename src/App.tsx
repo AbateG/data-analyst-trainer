97ed00442503659e59864ad51102e306c5ce6b7c
Daniel Abate <abate.daniel@gmail.com>
2025-09-06 19:00:12 -0400

    fix: remove nested Router to prevent double BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import SqlPage from './pages/SqlPage';
@@ -10,20 +10,18 @@ import './styles/App.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/sql" element={<SqlPage />} />
-            <Route path="/python" element={<PythonPage />} />
-            <Route path="/conceptual" element={<ConceptualPage />} />
-            <Route path="/progress" element={<ProgressPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sql" element={<SqlPage />} />
+          <Route path="/python" element={<PythonPage />} />
+          <Route path="/conceptual" element={<ConceptualPage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
